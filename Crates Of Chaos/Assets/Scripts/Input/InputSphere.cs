c8c2f5afeec1c9e1b70b9fda3508dcbcf3fec932
Derek Schindhelm <ddschindhelm@gmail.com>
2016-07-06 09:41:37 +0200

    Changed the way crystals are collected a bit

--- Crates Of Chaos/Assets/Scripts/Input/InputSphere.cs
+++ Crates Of Chaos/Assets/Scripts/Input/InputSphere.cs
@@ -2,10 +2,25 @@
 using System.Collections;
 
 public class InputSphere : MonoBehaviour {
+    public InputManager manager {get; set;}
+    private const float MAX_SIZE = 3f;
+    private const float MIN_SIZE = 0.5f;
+    private const float GROWTH_SPEED = 5f;
+
     private Vector3 currentTarget;
 
     private void Update() {
-
+        // Scale will always be equal in all dimensions so it's ok to just look at the x scale
+        float currentScale = transform.localScale.x;
+        if (currentScale < MAX_SIZE)
+        {
+            currentScale += Time.deltaTime * GROWTH_SPEED;
+            if (currentScale > MAX_SIZE)
+                currentScale = MAX_SIZE;
+            transform.localScale = new Vector3(currentScale, currentScale, currentScale);
+        } else {
+            Destroy(gameObject);
+        }
     }
 
     public void OnTriggerEnter2D(Collider2D other) {
@@ -15,12 +30,16 @@ public class InputSphere : MonoBehaviour {
     }
 
     private void CrateCollision(GameObject crate) {
-        SendMessageUpwards("CrateSelected", crate);
+        manager.CrateSelected(crate);
     }
 
     public void SetCurrentTarget(Vector3 position) {
         position.z = 0;
         currentTarget = position;
         transform.position = position;
+    }
+
+    void OnEnable() {
+        transform.localScale = new Vector3(MIN_SIZE, MIN_SIZE, MIN_SIZE);
     }
 }
