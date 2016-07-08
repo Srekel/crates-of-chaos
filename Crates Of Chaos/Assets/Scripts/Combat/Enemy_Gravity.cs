f81e8c2740b02e388cfc8d96cdd9046e79c7929f
Anders Elfgren <srekel@gmail.com>
2016-07-08 14:54:02 +0200

    assets and attempt at gravity enemy

---
+++ Crates Of Chaos/Assets/Scripts/Combat/Enemy_Gravity.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Enemy_Gravity : MonoBehaviour {
+	List<Rigidbody2D> objects = new List<Rigidbody2D>();
+
+
+	void FixedUpdate() {
+		foreach (Rigidbody2D rb in objects)
+		{
+			Debug.DrawLine(transform.position, rb.transform.position);
+			// calculate direction from target to me
+			Vector3 forceDirection = transform.position - rb.transform.position;
+
+			// apply force on target towards me
+			rb.AddForce(forceDirection.normalized * 1000 * Time.fixedDeltaTime);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		var rb = collider.gameObject.GetComponent<Rigidbody2D>();
+		if (rb)
+		{
+			objects.Add(rb);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D collider)
+	{
+		var rb = collider.gameObject.GetComponent<Rigidbody2D>();
+		if (rb)
+		{
+			objects.Remove(rb);
+		}
+	}
+}
